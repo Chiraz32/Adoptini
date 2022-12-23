129d36915af1b3657b4f84fd795630ca29344345
doss chiraz <doss.chiraz13@gmail.com>
2022-12-23 15:56:43 +0100

    adding repositories and adoption form

---
+++ Controllers/AdoptingController.cs
@@ -0,0 +1,47 @@
+﻿using AnimalAdoption.DB_Connection;
+using AnimalAdoption.DB_Connection.Repositories;
+using AnimalAdoption.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalAdoption.Controllers
+{
+    public class AdoptingController : Controller
+    {
+        public IActionResult Adopt()
+        {
+            Debug.WriteLine(" form loading");
+            return View();
+        }
+        public IActionResult Afficher()
+        {
+            Debug.WriteLine("in afficher " );
+         
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Adopt( string name ,string surname, string email,string adress,string profession, string family_situation,int age,string phone_number)
+        {
+            Debug.WriteLine(" form arrived");
+            DataBaseContext dataBaseContext = DataBaseContext.Instantiate_DataBaseContext();
+            List<User> users = DataBaseContext.dataBase_Context.user.ToList();
+            int id = users.Count();
+            
+            User user = new User(id+1, name, surname, email, adress, profession, family_situation, age, phone_number);
+            Debug.WriteLine("user added  "+ user.Name + user.Id+ user.Family_situation);
+            UserRepository userRepository = new UserRepository(dataBaseContext);
+            userRepository.Add(user);
+            IEnumerable<User> usersList = userRepository.GetAll();
+            Debug.WriteLine(" userlist " +usersList.Count());
+
+            return RedirectToAction("Afficher");
+        }
+    }
+}
---
+++ DB Connection/Repositories/Repository.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AnimalAdoption.DB_Connection.Repositories
+{
+    public class Repository<TEntity>: IRepository<TEntity> where TEntity: class
+    {
+        private readonly DataBaseContext _dataBaseContext;
+        public Repository(DataBaseContext dataBaseContext)
+        {
+            this._dataBaseContext = dataBaseContext;
+        }
+
+        public bool Add(TEntity entity)
+        {
+            try
+            {
+                _dataBaseContext.Set<TEntity>().Add(entity);
+                _dataBaseContext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public bool AddRange(IEnumerable<TEntity> entities)
+        {
+            try
+            {
+                _dataBaseContext.Set<TEntity>().AddRange(entities);
+                _dataBaseContext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dataBaseContext.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return _dataBaseContext.Set<TEntity>().Find(id);
+
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dataBaseContext.Set<TEntity>().ToList();
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            try
+            {
+                _dataBaseContext.Set<TEntity>().Remove(entity);
+                _dataBaseContext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public bool RemoveRange(IEnumerable<TEntity> entities)
+        {
+            try
+            {
+                _dataBaseContext.Set<TEntity>().RemoveRange(entities);
+                _dataBaseContext.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
